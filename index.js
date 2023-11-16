a8a11402e734685138c88c0604aad02c623af44d
msahrana <msahrana@gmail.com>
2023-11-16 22:35:10 +0600

    bistro boss server site is create successfully

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crgl3kb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const menuCollection = client.db("bistroDB").collection("menu");
+
+    app.get('/menu', async(req, res) => {
+        const result = await menuCollection.find().toArray()
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    res.send('Bistro Boss server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Bistro Boss Restaurant server is running on port: ${port}`)
+})
