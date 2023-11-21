b61af6e9135668f7714ffb86deb14a1e1d80367a
msahrana <msahrana@gmail.com>
2023-11-21 13:08:41 +0600

    all ok

--- index.js
+++ index.js
@@ -23,10 +23,24 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray()
         res.send(result)
+    })
+
+    app.get('/reviews', async(req, res) => {
+        const result = await reviewCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/carts', async(req, res) => {
+      const cartItem = req.body 
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+
     })
 
     await client.db("admin").command({ ping: 1 });
